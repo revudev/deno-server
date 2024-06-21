32c734ba9ed19e7e58ca6b6e1c66e04a5caed07c
revudev <denys.msi04@gmail.com>
2024-06-21 13:03:24 +0200

    generate qrcode

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import { Application } from "https://deno.land/x/abc@v1.3.3/mod.ts";
+import { qrcode } from "https://deno.land/x/qrcode/mod.ts";
 
 const app = new Application();
 
@@ -7,3 +8,6 @@ app
     return "Hello, Abc!";
   })
   .start({ port: 8080 });
+
+const imgSrc = await qrcode(Deno.args[0]);
+Deno.writeTextFile("qrcode.html", `<img src='${imgSrc}' />`);
